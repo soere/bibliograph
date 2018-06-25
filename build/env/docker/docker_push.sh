27ffdf177df5b6bd12db051272e27d843c857708
Christian M. Boulanger <info@bibliograph.org>
2018-06-25 11:13:26 +0200

    11:13

--- build/env/docker/docker_push.sh
+++ build/env/docker/docker_push.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-REPO=$DOCKER_USERNAME/bibliograph
+REPO=$(echo "$DOCKER_USERNAME")/bibliograph
 TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 echo " >>> Building image $REPO:$TAG' ..."
 docker build -f ./build/env/Dockerfile -t $REPO:$TAG .
