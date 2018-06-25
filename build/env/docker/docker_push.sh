dcf316c99dfb74ae0cc35eea02d39c4010f217af
Christian M. Boulanger <info@bibliograph.org>
2018-06-25 11:58:32 +0200

    11:58

--- build/env/docker/docker_push.sh
+++ build/env/docker/docker_push.sh
@@ -2,7 +2,7 @@
 REPO=cboulanger/bibliograph
 TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 echo " >>> Building image $REPO:$TAG' ..."
-docker build -f ./build/env/Dockerfile -t $REPO:$TAG .
+docker build -f ./build/env/docker/Dockerfile -t $REPO:$TAG .
 echo " >>> Pushing to Docker hub...."
 echo "$DOCKER_PASSWORD" | docker login -u cboulanger --password-stdin
 docker push $REPO
